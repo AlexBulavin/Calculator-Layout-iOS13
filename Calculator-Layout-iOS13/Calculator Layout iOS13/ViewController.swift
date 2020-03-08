3c30a23cec3b0f08e998c7cb6e87b3e21e18307c
AlexBulavin <45867076+AlexBulavin@users.noreply.github.com>
2020-03-08 14:20:13 +0300

    Исправлены работа с операторами и адаптация шрифта

--- Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
+++ Calculator-Layout-iOS13/Calculator Layout iOS13/ViewController.swift
@@ -11,16 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentDisplay = "0"
-    var currentValue = 0
-    var memory1Value = 0
-    var memory2Value = 0
+    var currentValue = 0.00
+    var memory1Value = 0.00
+    var memory2Value = 0.00
     var oper = "0"
     var expectation = 1 //0 если нажата кнопка = и 1 в остальных случаях
-    var decemal = 0 //0 если число целое и 1 если десятичное
+    var decimal = 0 //0 если при наборе числа не была нажата точка и 1 если была нажата
+    var decimalMemo = 0 //0 если при наборе числа не была нажата точка и 1 если была нажата
+    var decimalRange = 1.00 //счетчик разрядности числа
+    var addedOrSubtractedValue = 0.00
     
-        override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        displayLable.text = String (0)
     }
     @IBOutlet weak var displayLable: UILabel!
     @IBAction func buttonClicked(_ sender: UIButton) {
@@ -30,75 +33,111 @@ class ViewController: UIViewController {
         
         switch clickedButton {
         case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
-            if expectation == 0 {
-            currentValue = Int(clickedButton)!
-            currentDisplay = String(currentValue)
+            if String(currentValue).count > 12 {displayLable.font = UIFont.systemFont(ofSize: 30.0)} //Проверяем количество символов в строке currentValue. Если больше 12, уменьшаем размер шрифта
+            if decimal == 0 {currentValue = currentValue * 10 + Double(clickedButton)!
+                currentDisplay = String(Int(currentValue))
                 displayLable.text = currentDisplay
-                expectation = 1
+                addedOrSubtractedValue = currentValue
             }
-            else {
-                
-            currentValue = Int(currentDisplay + clickedButton)!
-            currentDisplay = String(currentValue)
+            else
+            { decimalRange = decimalRange/10
+                currentValue = currentValue + Double(clickedButton)! * decimalRange
+                currentDisplay = String(currentValue)
                 displayLable.text = currentDisplay
+                addedOrSubtractedValue = currentValue
             }
             
         case "+", "-", "×", "÷", "%" :
             oper = clickedButton
             memory1Value = currentValue
             currentValue = 0
-            currentDisplay = String(currentValue) + clickedButton
+            decimalMemo = decimal
+            decimal = 0
+            decimalRange = 1.00
+            currentDisplay = String(Int(currentValue))
             displayLable.text = currentDisplay
-                       
+            //addedOrSubtractedValue = currentValue
+            
         case "+/-":
             currentValue = currentValue * -1
-            currentDisplay = String(currentValue)
+            if String(currentValue).count > 12 {displayLable.font = UIFont.systemFont(ofSize: 30.0)}
+            if decimal == 1 {
+                currentDisplay = String(currentValue)}
+            else {currentDisplay = String(Int(currentValue))}
             displayLable.text = currentDisplay
             
         case "AC":
+            if String(currentValue).count > 11 {displayLable.font = UIFont.systemFont(ofSize: 50.0)}
             currentValue = 0
-            currentDisplay = String(currentValue)
+            decimal = 0
+            decimalRange = 1.00
+            addedOrSubtractedValue = 0.00
+            memory1Value = 0.00
+            currentDisplay = String(Int(currentValue))
             displayLable.text = currentDisplay
             
         case "=":
+            if String(currentValue).count > 11 {displayLable.font = UIFont.systemFont(ofSize: 30.0)}
             switch oper {
             case "+":
-                currentValue = currentValue + memory1Value
-                currentDisplay = String(currentValue)
+                currentValue = memory1Value + addedOrSubtractedValue
+                if decimal == 1 || decimalMemo == 1 {
+                    currentDisplay = String(currentValue)}
+                else {currentDisplay = String(Int(currentValue))}
                 displayLable.text = currentDisplay
                 expectation = 0
+                if decimalMemo == 1 {decimal = decimalMemo}
+                memory1Value = currentValue
             case "-":
-                currentValue = memory1Value - currentValue
-                currentDisplay = String(currentValue)
+                currentValue = memory1Value - addedOrSubtractedValue
+                if decimal == 1 || decimalMemo == 1 {
+                    currentDisplay = String(currentValue)}
+                else {currentDisplay = String(Int(currentValue))}
                 displayLable.text = currentDisplay
                 expectation = 0
+                if decimalMemo == 1 {decimal = decimalMemo}
+                memory1Value = currentValue
             case "×":
-                currentValue = currentValue * memory1Value
-                currentDisplay = String(currentValue)
+                currentValue = addedOrSubtractedValue * memory1Value
+                if decimal == 1 || decimalMemo == 1 {
+                    currentDisplay = String(currentValue)}
+                else {currentDisplay = String(Int(currentValue))}
                 displayLable.text = currentDisplay
                 expectation = 0
+                if decimalMemo == 1 {decimal = decimalMemo}
+                memory1Value = currentValue
             case "÷":
-                currentValue = memory1Value / currentValue
-                currentDisplay = String(currentValue)
+                currentValue = memory1Value / addedOrSubtractedValue
+                if currentValue > (memory1Value).truncatingRemainder(dividingBy: addedOrSubtractedValue)
+                { decimal = 1}
+                if decimal == 1 || decimalMemo == 1 {
+                    currentDisplay = String(currentValue)}
+                else {currentDisplay = String(Int(currentValue))}
                 displayLable.text = currentDisplay
                 expectation = 0
+                if decimalMemo == 1 {decimal = decimalMemo}
+                memory1Value = currentValue
             case "%":
-                currentValue = (memory1Value*100 / currentValue)
-                currentDisplay = String(currentValue)
+                currentValue = (memory1Value*100 / addedOrSubtractedValue)
+                if decimal == 1 || decimalMemo == 1 {
+                    currentDisplay = String(currentValue)}
+                else {currentDisplay = String(Int(currentValue))}
                 displayLable.text = currentDisplay
                 expectation = 0
-                
+                if decimalMemo == 1 {decimal = decimalMemo}
+                memory1Value = currentValue
             default: break}
             
-//        case "."
-//                currentValue = (memory1Value*100 / currentValue)
-                currentDisplay = String(currentValue) + "."
+            
+        case ".":
+            if decimal == 0 {
+                decimal = 1
+                currentDisplay = String(Int(currentValue)) + "."
                 displayLable.text = currentDisplay
-                expectation = 0
-                decemal = 1
+            }
             
         default: break
-
+            
         }
         
     }
